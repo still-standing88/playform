0cfcb0cb9a2fef282a824ae68ac84aeedb2174d3
Oussama Ben <earsvision1@gmail.com>
2026-07-02 19:40:13 +0100

    Player gui: fixed interfering behavior with hotkeys and default widget behavior

--- src/gui_controls/player_key_event_filter.py
+++ src/gui_controls/player_key_event_filter.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QSpinBox, QSlider, QListWidget, QPushButton
+from PySide6.QtWidgets import QSpinBox, QSlider, QListWidget, QPushButton, QToolButton
 from PySide6.QtCore import QObject, QEvent, Qt
 
 
@@ -7,24 +7,23 @@ class KeyEventFilter(QObject):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-    
+
     def eventFilter(self, watched, event):
         if event.type() == QEvent.Type.ShortcutOverride:
-            key = event.key() # type: ignore
-            
+            key = event.key()
 
             if isinstance(watched, (QSpinBox, QSlider, QListWidget)):
                 if key in [Qt.Key.Key_Up, Qt.Key.Key_Down, Qt.Key.Key_Left, Qt.Key.Key_Right]:
                     event.accept()
                     return True
-                    
-            if isinstance(watched, QPushButton):
-                if key == Qt.Key.Key_Space:
+
+            if isinstance(watched, (QPushButton, QToolButton)):
+                if key in (Qt.Key.Key_Space, Qt.Key.Key_Enter, Qt.Key.Key_Return):
                     event.accept()
                     return True
-            
+
             return False
-        
+
         return False
     
     def install_on_widgets(self, widgets):
