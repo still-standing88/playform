82707aac41274cd2df284c13266449bec3f24b8c
Oussama Ben <earsvision1@gmail.com>
2026-07-07 10:33:26 +0100

    hotkeys: re-enabled global hotkeys via keyboard module, marshalled through Qt HotkeyBridge signal for thread safety

--- src/app_config/key_config.py
+++ src/app_config/key_config.py
@@ -1,34 +1,59 @@
 import os
 import re
-#import keyboard
+import keyboard
 import configparser as cfgp
+
+from PySide6.QtCore import QObject, Signal
 
 from utilities.functions import get_app_path
 from app_constance import default_keys
+
+
+class HotkeyBridge(QObject):
+    triggered = Signal(str)
+
 
 key_dict = default_keys.key_dict.copy()
 hotkeys = {}
 hotkeys_funcs = {}
 key_config = cfgp.ConfigParser()
+_bridge: HotkeyBridge | None = None
 
-def modifyKey(section,hotkey,sequence):
+
+def _get_bridge() -> HotkeyBridge:
+    global _bridge
+    if _bridge is None:
+        _bridge = HotkeyBridge()
+    return _bridge
+
+
+def modifyKey(section, hotkey, sequence):
     key_config[section][hotkey] = sequence
-    if section == "global":
-        pass #keyboard.remove_hotkey(hotkeys.pop(hotkey))
-        #hotkeys[hotkey] = keyboard.add_hotkey(sequence,hotkeys_funcs[hotkey])
-        
+    if section.lower() == "global":
+        old_id = hotkeys.pop(hotkey, None)
+        if old_id is not None:
+            try:
+                keyboard.remove_hotkey(old_id)
+            except Exception:
+                pass
+        bridge = _get_bridge()
+        hid = keyboard.add_hotkey(sequence, lambda n=hotkey: bridge.triggered.emit(n))
+        hotkeys[hotkey] = hid
+
 
 def is_valid_hotkey(hotkey):
     pattern = r"(alt|ctrl|shift|win)?\+?(alt|ctrl|shift|win)?\+?(alt|ctrl|shift|win)?\+?(space|enter|left|right|up|down|pageup|pagedown|home|end|\[|\]|f1|f2|f3|f4|f5|f6|f7|f8|f9|f10|f11|f12|\w)?"
-    match = re.match(pattern, hotkey.replace(" ","").lower())
+    match = re.match(pattern, hotkey.replace(" ", "").lower())
     return bool(match)
+
 
 def keysToDefault():
     global key_config
-    for keyset,keys in key_dict.items():
+    for keyset, keys in key_dict.items():
         key_config[keyset] = {}
-        for key  in keys:
+        for key in keys:
             key_config[keyset][key] = keys[key]
+
 
 def is_valid_config():
     for default_hotkeys_section, current_hotkeys_section in zip(list(default_keys.key_dict.keys()), list(key_config.keys())):
@@ -39,15 +64,17 @@ def is_valid_config():
                 return False
     return True
 
+
 def saveConfig():
     key_config_file = f"{get_app_path()}/data/key_config.cfg"
     with open(key_config_file, "w") as config_file:
         key_config.write(config_file)
 
+
 def load_keys():
     current_path = get_app_path()
     key_config_file = f"{current_path}/data/key_config.cfg"
-    if os.path.exists(key_config_file) == True: 
+    if os.path.exists(key_config_file):
         key_config.read(key_config_file)
         if not is_valid_config():
             keysToDefault()
@@ -57,14 +84,25 @@ def load_keys():
         saveConfig()
 
 
-
 def apply_global_hotkeys():
-    #for hotkey in hotkeys:
-        #keyboard.remove_hotkey(hotkeys[hotkey])
+    for hid in list(hotkeys.values()):
+        try:
+            keyboard.remove_hotkey(hid)
+        except Exception:
+            pass
     hotkeys.clear()
-    #for hotkey in key_dict["Global"]:
-        #hotkeys[hotkey] = keyboard.add_hotkey(key_config["Global"][hotkey],hotkeys_funcs[hotkey])
 
+    bridge = _get_bridge()
+    global_section = key_config.get("Global", {}) or key_dict.get("Global", {})
+    for action, sequence in global_section.items():
+        if not sequence:
+            continue
+        try:
+            name = action
+            hid = keyboard.add_hotkey(sequence, lambda n=name: bridge.triggered.emit(n))
+            hotkeys[action] = hid
+        except Exception:
+            pass
 
 
 def initialize(func_dict):
--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -361,6 +361,9 @@ class MainWindow(QMainWindow):
         self.player_widget.muteStateChanged.connect(self.menu_manager.update_media_mute_state)
         self.player_widget.mediaAvailable.connect(self.menu_manager.update_media_available)
         self.player_widget.repeatModeChanged.connect(self.menu_manager.update_media_repeat_mode)
+
+        from app_config.key_config import _get_bridge
+        _get_bridge().triggered.connect(self._on_global_hotkey_triggered)
             
     def open_file_dialog(self):
         file_path, selected_filter = QFileDialog.getOpenFileName(
@@ -565,6 +568,11 @@ class MainWindow(QMainWindow):
                 return
 
             speech_manager.output(message, prefs.prefs["tts_speech_interrupt"])
+
+    def _on_global_hotkey_triggered(self, name: str):
+        handler = self.global_hotkeys.get(name)
+        if handler:
+            handler()
 
     def toggle_play_pause(self):
         if hasattr(self.player_widget, 'player') and self.player_widget.player:
