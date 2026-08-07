b242af7a89f41d27b323147963098a9344dc682b
Oussama Ben <earsvision1@gmail.com>
2026-08-07 23:23:39 +0100

    Repoint tools package init to new thumbnail_generator module

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,7 +1,7 @@
 from tools.ffmpeg.batch_converter.ui import BatchConverterUI
 from tools.ffmpeg.media_extractor.ui import ExtractorUI
 from .tag_editor_ui import TagEditorUI
-from .thumbnail_generator_ui import ThumbnailGeneratorUI
+from tools.ffmpeg.thumbnail_generator.ui import ThumbnailGeneratorUI
 from .subtitle_converter_ui import SubtitleConverterUI
 from .subtitle_editor_ui import SubtitleEditorUI
 
