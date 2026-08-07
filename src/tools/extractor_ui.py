9d78707b06d47bc0c62dfac927510073468c3969
Oussama Ben <earsvision1@gmail.com>
2026-08-07 23:03:05 +0100

    Repoint extractor_ui.py to media_core.ffmpeg

--- src/tools/extractor_ui.py
+++ src/tools/extractor_ui.py
@@ -6,7 +6,7 @@ from PySide6.QtWidgets import (
 )
 from PySide6.QtCore import Qt, QThread, Signal
 from .ffmpeg_handler import FFmpegHandler
-from ffmpeg import FFmpegError, Progress
+from media_core.ffmpeg import FFmpegError, Progress
 from .utils import get_audio_formats_map, get_container_from_format
 from utilities import signal_manager
 
