56abc02f6c4a0b7855b3d83584073031879f8297
Oussama Ben <earsvision1@gmail.com>
2026-08-07 23:22:21 +0100

    Repoint tools package init to new media_extractor module

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,5 +1,5 @@
 from tools.ffmpeg.batch_converter.ui import BatchConverterUI
-from .extractor_ui import ExtractorUI
+from tools.ffmpeg.media_extractor.ui import ExtractorUI
 from .tag_editor_ui import TagEditorUI
 from .thumbnail_generator_ui import ThumbnailGeneratorUI
 from .subtitle_converter_ui import SubtitleConverterUI
