3c553ecfc91d7e0ec35d0eca705c45a3fb73c64d
Oussama Ben <earsvision1@gmail.com>
2026-08-08 18:31:29 +0100

    Wire tool_window_manager to new Audiobook Combiner

--- src/gui/managers/tool_window_manager.py
+++ src/gui/managers/tool_window_manager.py
@@ -7,6 +7,7 @@ from tools.subtitles.converter_ui import SubtitleConverterUI
 from tools.subtitles.editor_ui import SubtitleEditorUI
 from tools.speech_converter.ui import SpeechConverterUI
 from tools.m4b_tools.audiobook_tools.ui import AudiobookToolsUI
+from tools.m4b_tools.combiner.ui import AudiobookCombinerUI
 from gui.dialogs.tool_dialog import ToolDialog
 from tools.logs_viewer_dialog import LogsViewerDialog
 from utilities import signal_manager
@@ -47,6 +48,11 @@ class ToolWindowManager:
         if not ensure_ffmpeg_available(self.main_window, show_message=True, min_major=6):
             return
         self.open_tool_dialog("audiobook_tools", AudiobookToolsUI(), _("Audiobook Tools"))
+
+    def open_audiobook_combiner(self):
+        if not ensure_ffmpeg_available(self.main_window, show_message=True, min_major=6):
+            return
+        self.open_tool_dialog("audiobook_combiner", AudiobookCombinerUI(), _("Audiobook Combiner"))
 
     def open_tool_dialog(self, tool_name, tool_widget, title):
         if self.main_window.active_tool_name and self.main_window.active_tool_name != tool_name:
