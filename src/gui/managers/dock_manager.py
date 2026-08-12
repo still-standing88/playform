aeecf51c2d31ca971668e83eb4eb58305752bbbc
Oussama Ben <earsvision1@gmail.com>
2026-08-12 12:48:55 +0100

    dock_manager: connect podcast download signals on dock creation

--- src/gui/managers/dock_manager.py
+++ src/gui/managers/dock_manager.py
@@ -264,6 +264,8 @@ class DockManager:
         if self.main_window.podcast_dock is None:
             self.main_window.podcast_widget = FeedWidget(self.main_window)
             self.main_window.podcast_widget.play_requested.connect(self.main_window.urlOpened.emit)
+            self.main_window.podcast_widget.episode_download_requested.connect(self.main_window.queue_podcast_episode_download)
+            self.main_window.podcast_widget.episode_batch_download_requested.connect(self.main_window.queue_podcast_batch_download)
             self.main_window.podcast_dock = FloatableDockWidget(_("Podcasts"), self.main_window)
             self.main_window.podcast_dock.setObjectName("podcastDock")
             self.main_window.podcast_dock.setWidget(self.main_window.podcast_widget)
