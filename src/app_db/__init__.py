7a55ff8b50249697fea1bb59faa8a65c3e76ef85
Oussama Ben <earsvision1@gmail.com>
2026-08-12 12:34:01 +0100

    app_db: expose app_settings/user_presets singletons

--- src/app_db/__init__.py
+++ src/app_db/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from .user_db import UserFiles
 from .media_db import MediaDatabase
+from .settings_store import AppSettingsStore, PresetsStore
 from utilities.functions import get_app_path, get_user_directories
 
 current_path = get_app_path()
@@ -9,6 +10,9 @@ db_path = f"{current_path}/data/db.sqlite3"
 first_launch = True if not os.path.exists(db_path) else False
 user_db = UserFiles(db_path, simple_mode=True)
 user_db.connect_to_database()
+
+app_settings = AppSettingsStore(user_db)
+user_presets = PresetsStore(user_db)
 
 media_db_path = f"{current_path}/data/media.sqlite3"
 media_index_db_path = f"{current_path}/data/media_index.sqlite3"
