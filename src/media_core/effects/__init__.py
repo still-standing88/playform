8b60962d79e53306feabaf8b0867d553abbeb7ee
Oussama Ben <earsvision1@gmail.com>
2026-08-23 11:28:59 +0100

    Add media_core.effects package

---
+++ src/media_core/effects/__init__.py
@@ -0,0 +1,10 @@
+from .unified_catalog import (
+    UnifiedEffect,
+    UNIFIED_EFFECTS,
+    UNIFIED_EFFECTS_BY_ID,
+    get_unified_effect,
+    unified_categories,
+    effects_for_category,
+    live_effects,
+    offline_effects,
+)
